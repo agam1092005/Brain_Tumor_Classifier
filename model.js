d5ace8b360fcd6e05c3c960527afbd7d153fcfdd
Agampreet Singh <70815441+agam1092005@users.noreply.github.com>
2023-08-15 16:24:30 +0530

    Delete model.js

--- model.js
+++
@@ -1,7 +0,0 @@
-async function loadModel() {
-  const model = await tf.loadLayersModel('https://drive.google.com/file/d/1JYtPgYMUZsG192TYBeRHtvCOdVrmK1ey/view');
-
-  console.log(model);
-}
-
-loadModel();
